164476415b28e66eff6fc4d9d8929e0999dbacb0
Miranda Huwe <mirandahuwe@Mirandas-iMac-2.local>
2022-04-27 21:28:28 -0700

    added page background and header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 function App() {
   return (
     <div>
+      <header class='header'>
+        <div class='logo'>
+          <img id='logo_img'></img>
+          <img id='logo_text'></img>
+        </div>
+      </header>
 
     </div>
   );
