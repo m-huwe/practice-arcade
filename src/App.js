752be8bc0b1f46f3a3a44c10da56d7122bab865b
Miranda Huwe <mirandahuwe@Mirandas-iMac-2.local>
2022-04-27 22:07:52 -0700

    added logos to header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import logo from './Images/Logo_Alt.png';
+import logoText from './Images/Logo_Text.png';
 
 function App() {
   return (
     <div>
       <header class='header'>
         <div class='logo'>
-          <img id='logo_img'></img>
-          <img id='logo_text'></img>
+          <img id='logo_img' src={logo} ></img>
+          <img id='logo_text' src ={logoText}></img>
         </div>
       </header>
 
