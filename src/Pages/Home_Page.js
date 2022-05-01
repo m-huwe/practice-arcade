035807854c16cbf228a35e8071ce88099efa3872
Miranda Huwe <mhuwegriffin@outlook.com>
2022-04-30 19:13:35 -0700

    Added content to home page

--- src/Pages/Home_Page.js
+++ src/Pages/Home_Page.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../Styles/home.css';
+import sign from '../Images/Welcome.png';
+
 
 function Home_Page() {
   
   return (
-    <div></div>
+    <div>
+      <div className='container'>
+        <img id='sign' src={sign}/>
+        <h1>To play: </h1>
+        <p>Select a game from the navigation above.</p>
+      </div>
+    </div>
   )
 }
 
