0eac542d765a1011dbaf22e04cbeaba3ed42f1c5
Miranda Huwe <mhuwegriffin@outlook.com>
2022-05-01 12:58:51 -0700

    added grid to about page

--- src/Pages/About_Page.js
+++ src/Pages/About_Page.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import '../Styles/about.css';
 
 function About_Page() {
-  
+
   return (
-    <div>Contact_Page</div>
+    <div className="parent">
+      <div className="div1">
+        
+      </div>
+      <div className="div2">
+        
+      </div>
+      <div className="div3">
+        
+      </div>
+    </div>
   )
 }
 
