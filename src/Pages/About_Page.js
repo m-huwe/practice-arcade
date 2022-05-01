3c90a5d9f15ac0ea4019485b34037ea09aa15e9c
Miranda Huwe <mhuwegriffin@outlook.com>
2022-05-01 13:39:38 -0700

    added content to about section

--- src/Pages/About_Page.js
+++ src/Pages/About_Page.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import '../Styles/about.css';
+import gameRoom from '../Images/gameRoom.png';
 
 function About_Page() {
 
   return (
     <div className="parent">
       <div className="div1">
-        
+        <h2>- Social -</h2>
       </div>
       <div className="div2">
-        
+        <h2>- About -</h2>
+        <p>Practice Arcade was designed by Miranda Huwe. This project was conceptualized to practice my HTML, CSS, JS, and React skills. It was inspired by the neon color pallet, the 80's, arcade games, and my love for design. </p>
+        <img className='image' src={gameRoom}/>
+        <p>Thanks for visiting and I hope you enjoy the site!</p>
       </div>
       <div className="div3">
-        
+        <h2>- Contact -</h2>
       </div>
     </div>
   )
